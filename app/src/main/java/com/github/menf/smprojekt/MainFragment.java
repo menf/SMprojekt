5c5e007b5a113fce39c3ff29437f1660082ef52c
menf <menfis666@gmail.com>
2017-01-20 00:00:20 +0100

    Initial commit

---
+++ app/src/main/java/com/github/menf/smprojekt/MainFragment.java
@@ -0,0 +1,44 @@
+package com.github.menf.smprojekt;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by menf on 2017-01-19.
+ */
+
+public  class MainFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public MainFragment() {
+    }
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static MainFragment newInstance() {
+        MainFragment fragment = new MainFragment();
+        Bundle args = new Bundle();
+      //  args.putInt(ARG_SECTION_NUMBER);
+      //  fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+
+        return rootView;
+    }
+}
